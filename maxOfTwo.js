01a4123f6e93f49a1ae1df20a5fa038781c3ade2
Adrie <adriebuendia@icloud.com>
2022-04-15 22:44:15 -0400

    first commit

---
+++ maxOfTwo.js
@@ -0,0 +1,23 @@
+// let x = 1
+// let y = 2
+
+// // Write your code below this line.
+
+// let z = Math.max (x , y)
+// console.log(z);
+
+//  different numbers
+
+// let x = 50
+// let y = 20
+
+// let z = Math.max (x , y)
+// console.log(z)
+
+
+// same value
+let x = 10
+let y = 10
+
+let z = Math.max (x , y)
+console.log(z)
